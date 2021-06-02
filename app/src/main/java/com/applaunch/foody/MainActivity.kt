c17d7bfde84dfef379a34c9279beaec2add3c6cc
ashokmachineni <ashok5679m@gmail.com>
2021-06-02 12:51:16 +0100

    added nav graph and bottom nav menu

--- app/src/main/java/com/applaunch/foody/MainActivity.kt
+++ app/src/main/java/com/applaunch/foody/MainActivity.kt
@@ -2,10 +2,28 @@ package com.applaunch.foody
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.NavController
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
+import kotlinx.android.synthetic.main.activity_main.*
+
 //https://api.spoonacular.com/recipes/complexSearch?type=drink&diet=vegan&addRecipeInformation=true&number=1&fillIngredients=true&apiKey=367f74632264476797464bd574e056f2
 class MainActivity : AppCompatActivity() {
+    private lateinit var navController: NavController
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+         navController = findNavController(R.id.navHostFragment)
+        val appBuildConfig = AppBarConfiguration(setOf(R.id.recipesFragment,R.id.favRecipesFragment,R.id.jokesFragment))
+
+        bottomNavigationView.setupWithNavController(navController)
+        setupActionBarWithNavController(navController,appBuildConfig)
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        return navController.navigateUp() || super.onSupportNavigateUp()
     }
 }
